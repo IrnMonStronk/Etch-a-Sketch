fe09b4cc4006ba8b567c9d14011b99c703f8db66
Hristo Dikov <hristodikov@yahoo.com>
2022-05-30 23:04:46 +0200

    Trying to make it mobile-compatible

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ function createBoxes(numBox) {
     })
 //An eventListener for the hover effect.
     cells.forEach((box) => {
-        box.addEventListener('touchstart', () => {
+        box.addEventListener('touchmove', () => {
             if ( colorMode === 'noir' ) {
                 box.style.backgroundColor = 'black';
             }
@@ -60,7 +60,7 @@ function createBoxes(numBox) {
             }
         })
 //Added an eventListener remover to handle the mem leak i was experiencing
-        box.removeEventListener('touchstart', () => {
+        box.removeEventListener('touchmove', () => {
             if ( colorMode === 'noir' ) {
                 box.style.backgroundColor = 'black';
             }
